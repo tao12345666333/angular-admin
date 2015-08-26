f63c3e23c116085f8533456aa658bc239d785f97
ZhangJintao <tao12345666333@163.com>
2015-08-27 03:03:15 +0800

    fix module

--- js/app.js
+++ js/app.js
@@ -1,65 +1,26 @@
 'use strict';
 
-//var angularadminControllers = angular.module('angularadminControllers', []);
-
-//angularadminControllers.controller('SourceListCtrl', ['$scope', 'Source',
-    //function($scope, Source) {
-        //$scope.sources = [
-            //{
-                //'img': 'a.jpg',
-                //'tiltle': 'testTitle',
-                //'desc': 'testDesc',
-                //'author': 'testAuthor',
-                //'status': 'testStatus',
-                //'rank': 'testRank',
-                //'atime': 'testAtime'
-            //},
-            //{
-                //'img': 'a.jpg',
-                //'tiltle': 'testTitle1',
-                //'desc': 'testDesc1',
-                //'author': 'testAuthor1',
-                //'status': 'testStatus1',
-                //'rank': 'testRank1',
-                //'atime': 'testAtime1'
-            //}
-    //];
-//}])
-
-function SourceListCtrl ($scope) {
-    $scope.sources = [
-        {
-            'img': 'a.jpg',
-            'tiltle': 'testTitle',
-            'desc': 'testDesc',
-            'author': 'testAuthor',
-            'status': 'testStatus',
-            'rank': 'testRank',
-            'atime': 'testAtime'
-        },
-        {
-            'img': 'a.jpg',
-            'tiltle': 'testTitle1',
-            'desc': 'testDesc1',
-            'author': 'testAuthor1',
-            'status': 'testStatus1',
-            'rank': 'testRank1',
-            'atime': 'testAtime1'
-        }
-    ]
-}
-
-//angularAdmin = angular.module('angularAdmin', [
-    //'ngRoute',
-    //'angularadminControllers'
-//]);
-
-//angularAdmin.config(['$routeProvider',
-    //function($routeProvider) {
-        //$routeProvider.
-            //when('', {
-                //templateUrl: 'partials/source-list.html',
-                //controller: 'SourceListCtrl'
-            //})
-    //}    
-//])
+angular.module('angularAdmin', [])
+    .controller('SourceListController', function(){
+        var sourceList = this;
+        sourceList.sources = [
+            {
+                'img': 'a.jpg',
+                'title': 'testTitle',
+                'desc': 'testDesc',
+                'author': 'testAuthor',
+                'status': 'testStatus',
+                'rank': 'testRank',
+                'atime': 'testAtime'
+            },
+            {
+                'img': 'a.jpg',
+                'title': 'testTitle1',
+                'desc': 'testDesc1',
+                'author': 'testAuthor1',
+                'status': 'testStatus1',
+                'rank': 'testRank1',
+                'atime': 'testAtime1'
+            }
+        ];
+    });
