24effcd57b924ff707bb64216cd866245bbbae1e
ZhangJintao <tao12345666333@163.com>
2015-08-27 15:04:06 +0800

    DI

--- js/app.js
+++ js/app.js
@@ -1,54 +1,11 @@
 'use strict';
 
 angular.module('angularAdmin', [])
-    .controller('SourceListController', function(){
+    .controller('SourceListController', ['$http', function($http){
         var sourceList = this;
-        sourceList.sources = [
-            {
-                'img': 'a.jpg',
-                'title': 'testTitle4',
-                'desc': 'testDesc4',
-                'author': 'testAuthor4',
-                'status': 'testStatus4',
-                'rank': 4,
-                'atime': 'testAtime4'
-            },
-            {
-                'img': 'a.jpg',
-                'title': 'testTitle0',
-                'desc': 'testDesc0',
-                'author': 'testAuthor0',
-                'status': 'testStatus0',
-                'rank': 0,
-                'atime': 'testAtime0'
-            },
-            {
-                'img': 'a.jpg',
-                'title': 'testTitle2',
-                'desc': 'testDesc2',
-                'author': 'testAuthor2',
-                'status': 'testStatus2',
-                'rank': 2,
-                'atime': 'testAtime2'
-            },
-            {
-                'img': 'a.jpg',
-                'title': 'testTitle3',
-                'desc': 'testDesc3',
-                'author': 'testAuthor3',
-                'status': 'testStatus3',
-                'rank': 3,
-                'atime': 'testAtime3'
-            },
-            {
-                'img': 'a.jpg',
-                'title': 'testTitle1',
-                'desc': 'testDesc1',
-                'author': 'testAuthor1',
-                'status': 'testStatus1',
-                'rank': 1,
-                'atime': 'testAtime1'
-            }
-        ];
+
+        $http.get('http://127.0.0.1/angular').success(function(data){
+            sourceList.sources = data.res.sources
+        })
         sourceList.orderProp = 'rank'
-    });
+    }]);
