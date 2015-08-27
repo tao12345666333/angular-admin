b81ac95ac2f39e0c867af0655a4b900357cd79de
ZhangJintao <tao12345666333@163.com>
2015-08-27 12:34:48 +0800

    bi-directional data binding and repeat

--- js/app.js
+++ js/app.js
@@ -6,12 +6,39 @@ angular.module('angularAdmin', [])
         sourceList.sources = [
             {
                 'img': 'a.jpg',
-                'title': 'testTitle',
-                'desc': 'testDesc',
-                'author': 'testAuthor',
-                'status': 'testStatus',
-                'rank': 'testRank',
-                'atime': 'testAtime'
+                'title': 'testTitle4',
+                'desc': 'testDesc4',
+                'author': 'testAuthor4',
+                'status': 'testStatus4',
+                'rank': 4,
+                'atime': 'testAtime4'
+            },
+            {
+                'img': 'a.jpg',
+                'title': 'testTitle0',
+                'desc': 'testDesc0',
+                'author': 'testAuthor0',
+                'status': 'testStatus0',
+                'rank': 0,
+                'atime': 'testAtime0'
+            },
+            {
+                'img': 'a.jpg',
+                'title': 'testTitle2',
+                'desc': 'testDesc2',
+                'author': 'testAuthor2',
+                'status': 'testStatus2',
+                'rank': 2,
+                'atime': 'testAtime2'
+            },
+            {
+                'img': 'a.jpg',
+                'title': 'testTitle3',
+                'desc': 'testDesc3',
+                'author': 'testAuthor3',
+                'status': 'testStatus3',
+                'rank': 3,
+                'atime': 'testAtime3'
             },
             {
                 'img': 'a.jpg',
@@ -19,8 +46,9 @@ angular.module('angularAdmin', [])
                 'desc': 'testDesc1',
                 'author': 'testAuthor1',
                 'status': 'testStatus1',
-                'rank': 'testRank1',
+                'rank': 1,
                 'atime': 'testAtime1'
             }
         ];
+        sourceList.orderProp = 'rank'
     });
