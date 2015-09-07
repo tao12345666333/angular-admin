ce27fb208280ea49ec5aa13c2aef5f23e032df31
ZhangJintao <tao12345666333@163.com>
2015-09-07 16:52:35 +0800

    fix video

--- js/AngularAdmin/controllers.js
+++ js/AngularAdmin/controllers.js
@@ -1,15 +1,19 @@
 'use strict';
 
 angular.module('angularAdmin.controllers', [])
-    .controller('SourceListController', ['$scope', 'Sources', function($scope, Sources){
+    .controller('SourceListController', ['$scope', '$sce', 'Sources', function($scope, $sce, Sources){
         Sources.get(function(data){
+            angular.forEach(data.res, function(v, i){
+                v.src = $sce.trustAsResourceUrl(v.src);
+            });
             $scope.sources = data.res;
         });
 
         $scope.orderProp = 'atime';
     }])
-    .controller('SourceDetailController', ['$scope', '$stateParams', 'Source', function($scope, $stateParams, Source){
+    .controller('SourceDetailController', ['$scope', '$stateParams', '$sce', 'Source', function($scope, $stateParams, $sce, Source){
         Source.get({sourceId: $stateParams.sourceId}, function(data){
             $scope.source = data.res;
+            $scope.videosrc = $sce.trustAsResourceUrl(data.res.src);
         });
     }]);
