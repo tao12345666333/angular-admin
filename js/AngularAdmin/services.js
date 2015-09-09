6a398be5aee025f6c39da5818c825538aedd3695
ZhangJintao <tao12345666333@163.com>
2015-09-09 10:13:09 +0800

    fix

--- js/AngularAdmin/services.js
+++ js/AngularAdmin/services.js
@@ -2,12 +2,12 @@
 
 angular.module('angularAdmin.services', ['ngResource'])
     .factory('Sources', function($resource){
-        return $resource('../../data.json', {}, {
+        return $resource('data.json', {}, {
             get: {method: 'GET', params: {}, isArray: false}
         });
     })
     .factory('Source', function($resource){
         return $resource(':sourceId.json', {sourceId: '@sourceId'}, {
-            get: {method: 'GET', isArray: false}
+            get: {method: 'GET', isArray: false},
         });
     });
